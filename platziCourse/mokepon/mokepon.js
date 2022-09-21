65660c54cc6ef105e002f210f638c3f7493ce027
Marcwrz123 <marcoadrianzambra@gmail.com>
2022-09-20 22:35:34 -0500

    manioulacion del DOM en seleccion de mascotas del jugador y el enmigo

--- platziCourse/mokepon/mokepon.js
+++ platziCourse/mokepon/mokepon.js
@@ -1,37 +1,71 @@
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+}
+
+function seleccionarMascotaJugador() {
+    //mascotas
     let hipodoge = document.getElementById('hipodoge');
     let capipepo = document.getElementById('capipepo');
     let ratigueya = document.getElementById('ratigueya');
     let langostelvis = document.getElementById('langostelvis');
     let tucapalma = document.getElementById('tucapalma');
     let pydos = document.getElementById('pydos');
-}
-function seleccionarMascotaJugador() {
+    let spanMascotaJugador = document.getElementById("mascota-jugador");    //span
+    
     switch (true) {
         case hipodoge.checked:
-            console.log('hi1');
+            spanMascotaJugador.innerHTML = 'Hipodoge';
             break;
-        case capipepo.checked:
-            console.log('hi2');
-            break;
+            case capipepo.checked:
+                spanMascotaJugador.innerHTML = 'Capipepo';
+                break;
         case ratigueya.checked:
-            console.log('hi3');
+            spanMascotaJugador.innerHTML = 'Ratigueya';
             break;
         case langostelvis.checked:
-            console.log('hi4');
+            spanMascotaJugador.innerHTML = 'Langostelvis';
             break;
         case tucapalma.checked:
-            console.log('hi5');
+            spanMascotaJugador.innerHTML = 'Tucapalma';
             break;
         case pydos.checked:
-            console.log('hi6');
+            spanMascotaJugador.innerHTML = 'Pydos';
             break;                   
         default:
-            console.log('Not Really');
+            spanMascotaJugador.innerHTML = '';
+            break;
+        }
+        seleccionarMascotaEnemigo();
+    }
+    
+    function seleccionarMascotaEnemigo() {
+        let spanMascotaEnemigo = document.getElementById('mascota-enemigo');    //span
+        let ataqueAleatorio = aleatorio(1,6) //1H, 2C, 3R, 4L, 5T, 6P
+        switch (ataqueAleatorio) {
+            case 1:
+                spanMascotaEnemigo.innerHTML = 'Hipodoge';
+            break;
+        case 2:
+            spanMascotaEnemigo.innerHTML = 'Capipepo';
+            break;
+        case 3:
+            spanMascotaEnemigo.innerHTML = 'Ratigueya';
+            break;
+        case 4:
+            spanMascotaEnemigo.innerHTML = 'Langostelvis';
+            break;        
+        case 5:
+            spanMascotaEnemigo.innerHTML = 'Tucapalma';
+            break;
+        case 6:
+            spanMascotaEnemigo.innerHTML = 'Pydos';
             break;
     }
+}
+
+function aleatorio(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', iniciarJuego);
